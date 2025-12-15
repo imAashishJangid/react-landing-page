6ef4c993cc1a822deac6713a2bf1433735df7c9a
ImAashishjangid <github.com>
2025-12-16 00:13:29 +0530

    fix: vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/react-landing-page/', // 👈 use your repo name here
   plugins: [react()],
-})
+  base: "./",   // ⭐ IMPORTANT
+});
